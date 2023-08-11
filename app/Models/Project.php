45badf67064890c66da42fad69a8b9739be8f311
Ayomide Akinola <drayfocus@gmail.com>
2023-08-11 21:59:14 +0100

    feat: project entry category

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Project extends Model
 {
@@ -15,6 +16,8 @@ class Project extends Model
      * @var array
      */
     protected $guarded = [];
+
+    protected $appends = ['user_entry'];
 
     /**
      * Get the route key for the liquidation.
@@ -39,6 +42,11 @@ class Project extends Model
     public function entries()
     {
         return $this->hasMany(ProjectEntry::class);
+    }
+
+    public function getUserEntryAttribute()
+    {
+        return ProjectEntry::where('user_id', Auth::user()->id)->where('project_id', $this->id)->first();
     }
 
     public function milestones()
