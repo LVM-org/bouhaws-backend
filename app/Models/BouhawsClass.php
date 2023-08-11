c00b6b56a21fc8a610f805859e0e51e947f7d7cb
Ayomide Akinola <drayfocus@gmail.com>
2023-08-11 17:31:22 +0100

    feat: backend

--- app/Models/BouhawsClass.php
+++ app/Models/BouhawsClass.php
@@ -16,6 +16,8 @@ class BouhawsClass extends Model
      */
     protected $guarded = [];
 
+    protected $appends = ['students'];
+
     /**
      * Get the route key for the liquidation.
      *
@@ -36,7 +38,7 @@ class BouhawsClass extends Model
         return $this->hasMany(Project::class, 'bouhaws_class_id', 'id');
     }
 
-    public function students()
+    public function getStudentsAttribute()
     {
         $allEntriesUsers = ProjectEntry::whereIn('project_id', $this->projects()->pluck('id')->toArray())->pluck('user_id')->toArray();
 
