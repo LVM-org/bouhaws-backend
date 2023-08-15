de39e65baec306ce4787cce1af0c3579688fef54
Ayomide Akinola <drayfocus@gmail.com>
2023-08-15 14:53:10 +0100

    fix: websocket

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -11,8 +12,10 @@ use Illuminate\Support\Facades\Broadcast;
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
-*/
+ */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Route::post('/broadcasting/auth', 'BroadcastController@authenticate');
