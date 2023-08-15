9830f95bb1b514ec8c0551f20256fdbb434aada7
Ayomide Akinola <drayfocus@gmail.com>
2023-08-15 07:11:29 +0100

    fix: conversation

--- app/Models/ConversationMember.php
+++ app/Models/ConversationMember.php
@@ -28,6 +28,6 @@ class ConversationMember extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_uuid', 'uuid');
     }
 }
