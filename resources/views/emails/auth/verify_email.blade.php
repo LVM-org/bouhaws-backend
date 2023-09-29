98e4dcddb433607dd62f5d31ec51185abd00a2ac
Ayomide Akinola <drayfocus@gmail.com>
2023-09-29 23:57:55 +0100

    fix: mail

--- resources/views/emails/auth/verify_email.blade.php
+++ resources/views/emails/auth/verify_email.blade.php
@@ -1,12 +1,12 @@
 <x-mail::message>
-    Hi {{ $user->name }},
+  Hi {{ $user->name }},
 
-    Welcome to {{ config('app.name') }}! To secure your account, please verify your email by using this 4-digit code
+  Welcome to {{ config('app.name') }}! To secure your account, please verify your email by using this 4-digit code
 
-    ## {{ $user->otp }}
+  ## {{ $user->otp }}
 
-    This code will expire in 30 minutes, so please use it promptly.
+  This code will expire in 30 minutes, so please use it promptly.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+  Thanks,<br>
+  {{ config('app.name') }}
 </x-mail::message>
