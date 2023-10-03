845d602cb4bd2b14a30a823d4bece04723f66fd6
Ayomide Akinola <drayfocus@gmail.com>
2023-10-03 01:25:42 +0100

    feat: fund wallet

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,12 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
+use Laravel\Cashier\Billable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-    use Authenticatable, Authorizable, HasFactory, HasUuid, CanResetPassword;
+    use Authenticatable, Authorizable, HasFactory, HasUuid, CanResetPassword, Billable;
 
     /**
      * The attributes that are mass assignable.
