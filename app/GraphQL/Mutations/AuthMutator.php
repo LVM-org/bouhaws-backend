98059c32228c1858b6a10c5034ea333ad1898c49
Ayomide Akinola <drayfocus@gmail.com>
2024-06-17 13:57:19 +0100

    fix: google auth

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -74,8 +74,8 @@ final class AuthMutator
     public function googleAuth($_, array $args)
     {
         $token = $args['token'];
-        $access_token = Socialite::driver('google')->getAccessTokenResponse($token);
-        $user = Socialite::driver('google')->userFromToken($access_token['access_token']);
+
+        $user = Socialite::driver('google')->stateless()->userFromToken($token);
 
         $existingUser = User::where('email', $user->getEmail())->first();
 
